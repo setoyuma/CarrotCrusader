c6daf61ba3a835c40ce2caa9575b4825d018e8c4
seto yuma <setoichi.dev@gmail.com>
2023-02-19 18:45:31 -0800

    cleaned up dir

--- assets/Levels/TileSets/terrainTiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="terrainTiles" tilewidth="32" tileheight="32" tilecount="42" columns="6">
- <image source="../../terrain/terrainTiles.png" width="192" height="224"/>
-</tileset>
